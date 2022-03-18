d6f9cd7d507aa1ef335f88f8145477a1b8f8d278
unknown <akbiyik.ilker7@gmail.com>
2022-03-18 15:22:21 +0100

    initial state

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Iterator;
+
+public class Main <T> implements Iterable <T> {
+	public static void main(String[] args) {
+		System.out.println("create the dynamic array here");
+		
+		
+		
+		
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
